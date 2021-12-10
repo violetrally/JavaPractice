98a4138ade0f1b34f0a337c5299a713419e393ff
violetrally <violetrally@gmail.com>
2021-12-10 16:54:49 -0500

    done test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Let's say Hello in our new repository!
+
+class HelloWorld
+{
+    // all we need is our main method
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
